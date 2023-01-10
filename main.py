c223b309061bc950d8a2cbf8ed3756bdd953c238
Wissididom <webmaster@wissididom.me>
2023-01-10 17:40:56 +0100

    Split test and bot execution into two separate files

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from bot import Bot
-# import api as SafeGIF
 from dotenv import load_dotenv
 import os
 import webbrowser
@@ -8,9 +7,6 @@ load_dotenv()
 
 
 def main():
-    # print(SafeGIF.process_gif('epilepsy.gif'))
-    # print(SafeGIF.process_gif('normal.gif'))
-    # print(SafeGIF.process_gif('https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_a770d1805b514b97956c9695508e0d44/default/dark/3.0'))
     webbrowser.open(f"https://id.twitch.tv/oauth2/authorize?client_id={os.getenv('TWITCH_CLIENT_ID')}&redirect_uri={os.getenv('TWITCH_REDIRECT_URI')}&response_type=token&scope=chat%3Aread+chat%3Aedit+moderator%3Amanage%3Achat_messages")
     print("After you've authorized you get redirected to a non-existent page. Please copy+paste the url below!")
     url = input('Paste the url twitch redirects you to here: ')
