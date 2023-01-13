1593e629b1a1c6f68570e84f54bc39672ed8361e
Wissididom <webmaster@wissididom.me>
2023-01-13 18:15:04 +0100

    Fixed two missing renames

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ load_dotenv()
 
 TWITCH_CLIENT_ID = os.getenv('TWITCH_CLIENT_ID', 'byvo5xr9mepepi9km69nfab44jbglk')
 TWITCH_REDIRECT_URI = os.getenv('TWITCH_REDIRECT_URI', 'https://pasteme.local/')
-INITIAL_CHANNELS = os.getenv('INITIAL_CHANNEL')
+INITIAL_CHANNELS = os.getenv('INITIAL_CHANNELS')
 SCOPES = 'chat%3Aread+chat%3Aedit+moderator%3Amanage%3Achat_messages+user%3Amanage%3Awhispers'
 
 
 def main():
     global TWITCH_CLIENT_ID, TWITCH_REDIRECT_URI, INITIAL_CHANNELS, SCOPES
     if INITIAL_CHANNELS is None:
-        INITIAL_CHANNELS = input("Please enter the name of the channel you want the bot to monitor: ").strip()
+        INITIAL_CHANNELS = input("Please enter the name of the channels you want the bot to monitor (separated by comma): ").strip()
     webbrowser.open(f"https://id.twitch.tv/oauth2/authorize?client_id={TWITCH_CLIENT_ID}&redirect_uri={TWITCH_REDIRECT_URI}&response_type=token&scope={SCOPES}")
     print("After you've authorized you get redirected to a non-existent page. Please copy+paste the url below!")
     url = input('Paste the url twitch redirects you to here: ').strip()
