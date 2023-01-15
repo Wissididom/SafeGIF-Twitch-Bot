8f6de8bba22b12692237c34c68db20ff5e3c4a40
Simon Hahne <simonhahne@web.de>
2023-01-15 19:03:14 +0100

    Move test images into a test_images/ directory.

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 
 def main():
-    print(safegif.process_gif('epilepsy.gif'))
-    print(safegif.process_gif('normal.gif'))
+    print(safegif.process_gif('test_images/epilepsy.gif'))
+    print(safegif.process_gif('test_images/normal.gif'))
     print(safegif.process_gif(
         'https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_a770d1805b514b97956c9695508e0d44/default/dark/3.0'))
     print(safegif.process_gif('https://webaim.org/articles/seizure/media/flicker.gif'))
