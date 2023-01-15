a4d5afcd6504824672c19e739c18bc05b49cc9ad
NiceWaffel <56729720+NiceWaffel@users.noreply.github.com>
2023-01-15 18:53:25 +0100

    Use https instead of http for image links. (#7)

--- test.py
+++ test.py
@@ -10,8 +10,8 @@ def main():
     print(safegif.process_gif('normal.gif'))
     print(safegif.process_gif(
         'https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_a770d1805b514b97956c9695508e0d44/default/dark/3.0'))
-    print(safegif.process_gif('http://webaim.org/articles/seizure/media/flicker.gif'))
-    print(safegif.process_gif('http://webaim.org/articles/seizure/media/illusion.gif'))
+    print(safegif.process_gif('https://webaim.org/articles/seizure/media/flicker.gif'))
+    print(safegif.process_gif('https://webaim.org/articles/seizure/media/illusion.gif'))
 
 
 if __name__ == '__main__':
