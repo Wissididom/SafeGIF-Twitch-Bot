bddcea8b169771b11dc2be01f40173874faeb87f
Wissididom <webmaster@wissididom.me>
2023-01-22 14:43:52 +0100

    Made Test Output look nicer

--- test.py
+++ test.py
@@ -11,7 +11,11 @@ def main():
         ("https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_502bf8fd256b44348d9a5b9c546bee67/default/dark/3.0", True)
     ]
     for i in range(len(images)):
-        print(f"{images[i][1]} expected, got {safegif.process_gif(images[i][0])}")
+        expected = images[i][1]
+        got = safegif.process_gif(images[i][0])
+        success = expected is got
+        print(
+            f"{expected} expected, got {got}: {'✓' if success else '✖'}")
 
 
 if __name__ == '__main__':
