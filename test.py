aede36726f07223ebd5e8f7acae4c4c218928a39
Wissididom <webmaster@wissididom.me>
2023-01-22 14:29:58 +0100

    Changed test case to tuples

--- test.py
+++ test.py
@@ -3,15 +3,15 @@ import api as safegif
 
 def main():
     images = [
-        "test_images/epilepsy.gif",
-        "test_images/normal.gif",
-        "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_a770d1805b514b97956c9695508e0d44/default/dark/3.0",
-        "https://webaim.org/articles/seizure/media/flicker.gif",
-        "https://webaim.org/articles/seizure/media/illusion.gif",
-        "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_502bf8fd256b44348d9a5b9c546bee67/default/dark/3.0"
+        ("test_images/epilepsy.gif", True),
+        ("test_images/normal.gif", False),
+        ("https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_a770d1805b514b97956c9695508e0d44/default/dark/3.0", True),
+        ("https://webaim.org/articles/seizure/media/flicker.gif", True),
+        ("https://webaim.org/articles/seizure/media/illusion.gif", False),
+        ("https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_502bf8fd256b44348d9a5b9c546bee67/default/dark/3.0", True)
     ]
     for i in range(len(images)):
-        print(safegif.process_gif(images[i]))
+        print(f"{images[i][1]} expected, got {safegif.process_gif(images[i][0])}")
 
 
 if __name__ == '__main__':
